5a252e0bcce0cbd3ae9a132f4f747b696fc252b1
User <varmakalidindi27@gmail.com>
2021-01-09 23:57:40 +0200

    First Commit...

---
+++ src/pratice/Test1.java
@@ -0,0 +1,18 @@
+package pratice;
+
+public class Test1 {
+
+	
+	public static void main(String[] args) {
+
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+
+}
